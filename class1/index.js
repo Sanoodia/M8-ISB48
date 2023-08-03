9d066b9b758097a32359cb364939316303ed9f21
Sanoodia <sanoodia.micromerger@gmail.com>
2023-08-03 23:08:51 +0500

    class 1 code added

---
+++ class1/index.js
@@ -0,0 +1,21 @@
+// primitive                 /            non primitive
+// number, string, boolean        // Object, Array, Function
+var num1 = 20;
+console.log(num1);
+num1 = num1 + 30;
+var num2 = 10; //not changeable
+console.log(num1);
+var tellMeType = typeof (num2);
+console.log(tellMeType);
+var myName = "Sanoodia Asgher";
+console.log(myName);
+console.log(myName);
+console.log(myName);
+console.log(myName);
+console.log(myName);
+console.log(myName);
+console.log(myName + ":)" + myName);
+var yes = true;
+var no = false;
+console.log(yes);
+console.log(no);
---
+++ class1/index.ts
@@ -0,0 +1,31 @@
+
+// primitive                 /            non primitive
+// number, string, boolean        // Object, Array, Function
+
+let num1: number = 20;
+console.log(num1)
+
+num1 = num1 + 30;
+
+const num2 = 10; //not changeable
+
+
+console.log(num1)
+let tellMeType: string = typeof(num2)
+console.log(tellMeType)
+
+let myName: string = "Sanoodia Asgher";
+console.log(myName)
+console.log(myName)
+console.log(myName)
+console.log(myName)
+console.log(myName)
+console.log(myName)
+console.log(myName+":)"+myName)
+
+let yes :boolean = true;
+let no :boolean = false;
+
+console.log(yes)
+console.log(no)
+
